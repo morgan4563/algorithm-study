9a6aec5bfae5ebe5fbc60712ac3f6aac8d22af0e
HyunjunPark <142282197+morgan4563@users.noreply.github.com>
2025-01-07 07:18:50 +0900

    [Bronze V] Title: A+B - 5, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+while true {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    if input[0] == 0 && input[1] == 0 {
+        break
+    }
+    print(input[0] + input[1])
+}
