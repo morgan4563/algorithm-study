db7c87e51c000d6f3157c783557d4e77db5d11cf
Hyunjun Park <142282197+morgan4563@users.noreply.github.com>
2025-02-21 02:04:26 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+var input = readLine()!
+
+var result = [Int](repeating: -1, count: 26)
+var alpa = [
+    "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9,
+    "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19,
+    "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25
+]
+
+var index = 0
+for i in input {
+    if result[alpa[String(i)]!] == -1 {
+        result[alpa[String(i)]!] = index
+    }
+    index += 1
+}
+
+result.forEach { Int in
+    print(Int)
+}
