fbc73feb9a1b8da39cca281db39d2c474266bb77
Hyunjun Park <142282197+morgan4563@users.noreply.github.com>
2025-10-23 16:35:31 +0900

    [level 0] Title: OX퀴즈, Time: 0.27 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.swift
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.swift
@@ -1,45 +1,28 @@
 import Foundation
 
 func solution(_ quiz:[String]) -> [String] {
-    var resultArray = [String]()
-    
-    for i in quiz {
-        let data = i.split(separator: " ")
+    var result = [String]()
+    //퀴즈 반복 돌면서 하나씩 검증
+    for q in quiz {
+        //공백을 기준으로 나눠서 배열로 만듬
+        let qArr = q.split(separator: " ")
+        //-,+ 를 2번 인덱스의 앞에 붙친다음 Int로 변경
+        let first = Int(qArr[0])!
+        let second = Int(qArr[2])!
         
-        let num1 = data[0]
-        let exp = data[1]
-        let num2 = data[2]
-        let result = data[4]
-        
-        switch exp {
+        // 기호 별로 switch
+        switch qArr[1] {
             case "+":
-            if Int(num1)! + Int(num2)! == Int(result)! {
-                resultArray.append("O")
-            } else {
-                resultArray.append("X")
-            }
+            result.append(first+second == Int(qArr[4])! ? "O" : "X")
             case "-":
-            if Int(num1)! - Int(num2)! == Int(result)! {
-                resultArray.append("O")
-            } else {
-                resultArray.append("X")
-            }
-            case "/":
-            if Int(num1)! / Int(num2)! == Int(result)! {
-                resultArray.append("O")
-            } else {
-                resultArray.append("X")
-            }
-            case "*":
-            if Int(num1)! * Int(num2)! == Int(result)! {
-                resultArray.append("O")
-            } else {
-                resultArray.append("X")
-            }
+            result.append(first-second == Int(qArr[4])! ? "O" : "X")
             default:
             break
         }
+        
+        //위의 계산 결과를 4번 인덱스와 비교
+        
+        //true면 result에 "O"추가 false면 result에 "X"추가
     }
-    return resultArray
-    
+    return result
 }
